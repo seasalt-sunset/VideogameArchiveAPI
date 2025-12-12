e3832e5ddbf18748095f1b6f3debc81856941185
seasalt-sunset <sbreezerick@gmail.com>
2025-12-12 13:33:09 +0100

    Add Serilog

--- VideogameArchiveAPI/Program.cs
+++ VideogameArchiveAPI/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using VideogameArchiveAPI.Data;
+using Serilog;
+
 namespace VideogameArchiveAPI
 {
     public class Program
@@ -7,8 +11,16 @@ namespace VideogameArchiveAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/videogameApiLog.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
             // Add services to the container.
-
+            builder.Services.AddDbContext<VideogameArchiveAPIDbContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreConnection"));
+            });          
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
